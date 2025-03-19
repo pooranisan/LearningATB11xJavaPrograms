24efe34968f0eb54085831baa1c08d44d5129e3a
pooranisan <pooranisan@gmail.com>
2025-03-20 02:16:58 +0530

    11th March Assignment : Code for Multiplication of Table9

---
+++ src/ex_02_Java_Basics_part2/Printing_Table9_printf.java
@@ -0,0 +1,27 @@
+package ex_02_Java_Basics_part2;
+
+public class Printing_Table9_printf {
+
+    public static void main(String[] args) {
+
+        int t = 9;
+
+         System.out.printf("Displaying Multiplication Table of 9\n");
+
+         System.out.printf("Value of 9*1 = %d%n", t*1);
+         System.out.printf("Value of 9*2 = %d%n", t*2);
+         System.out.printf("Value of 9*3 = %d%n", t*3);
+         System.out.printf("Value of 9*4 = %d%n", t*4);
+         System.out.printf("Value of 9*5 = %d%n", t*5);
+         System.out.printf("Value of 9*6 = %d%n", t*6);
+         System.out.printf("Value of 9*7 = %d%n", t*7);
+         System.out.printf("Value of 9*8 = %d%n", t*8);
+         System.out.printf("Value of 9*9 = %d%n", t*9);
+         System.out.printf("Value of 9*10 = %d%n", t*10);
+
+
+    }
+
+
+
+}
