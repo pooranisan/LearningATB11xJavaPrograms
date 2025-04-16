3b280dcf49171bae736d69548de4ccc044b77981
pooranisan <pooranisan@gmail.com>
2025-04-16 21:05:42 +0530

    Multiplication table using for loop

---
+++ src/ex_02_Java_Basics_part2/Printing_table9_For_loop.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics_part2;
+
+public class Printing_table9_For_loop {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf("Printing Table 9 \n");
+
+        int number=9;
+
+        for (int i=1; i<=10; i++)
+        {
+            System.out.println(number + "*" +i + "=" + number * i);
+
+
+        }
+
+    }
+}
