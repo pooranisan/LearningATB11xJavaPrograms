7d602c15c16460455e70522a4c700050614b0c9a
pooranisan <pooranisan@gmail.com>
2025-04-22 00:32:31 +0530

    Practice Work 14th March 2025

---
+++ src/ex_05_Increment_Decrement_Operator/Lab_31_Iop_Dop.java
@@ -0,0 +1,24 @@
+package ex_05_Increment_Decrement_Operator;
+
+public class Lab_31_Iop_Dop {
+
+    public static void main(String[] args) {
+
+        //Increment operator
+        //int a= 5;
+        //int b = ++a; //Increment
+
+//        System.out.println(b);
+//        System.out.println(a);
+
+        //Post Increment operator
+
+        int a_post= 15;
+        int b_post = a_post++;
+
+        System.out.println(b_post);
+        System.out.println(a_post);
+        System.out.println(a_post++);
+
+    }
+}
