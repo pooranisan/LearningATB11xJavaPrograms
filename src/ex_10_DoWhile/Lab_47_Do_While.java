1eb15952c5fadbbc8dce8040f1b767fc4d5f0599
pooranisan <pooranisan@gmail.com>
2025-04-23 23:11:53 +0530

    Practice work 21st March 2025

---
+++ src/ex_10_DoWhile/Lab_47_Do_While.java
@@ -0,0 +1,18 @@
+package ex_10_DoWhile;
+
+public class Lab_47_Do_While
+{
+
+    public static void main(String[] args)
+    {
+        int a =1;
+        do
+        {
+            System.out.println("This is body, which will execute 1 time");
+            System.out.println(a);
+            a++;
+        }
+        while(a < 10);
+
+    }
+}
---
+++ src/ex_10_DoWhile/Lab_48_Diff_While_Dowhile.java
@@ -0,0 +1,29 @@
+package ex_10_DoWhile;
+
+public class Lab_48_Diff_While_Dowhile
+        {
+            public static void main(String[] args)
+            {
+             //int a = 0;
+                int number = 0;
+
+//             while (a <0)
+//             {
+//                 System.out.println(a);
+//                 a++;
+//             }
+//                do {
+//                    System.out.println(a);
+//                    a++;
+//                }
+//                while (a < 0);
+
+                do {
+                    System.out.println(number);
+                    number++;
+                }
+                while (number < 0);
+                }
+                }
+
+
