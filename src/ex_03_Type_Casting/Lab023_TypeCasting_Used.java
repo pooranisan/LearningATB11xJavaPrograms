60efc1ede35d6133b5d22293fdd3dcb8cf41c3ee
pooranisan <pooranisan@gmail.com>
2025-05-13 12:14:20 +0530

    Code updated

--- src/ex_03_Type_Casting/Lab023_TypeCasting_Used.java
+++ src/ex_03_Type_Casting/Lab023_TypeCasting_Used.java
@@ -6,13 +6,13 @@ public class Lab023_TypeCasting_Used {
 
         int course_fee = 100;
         float GST = 18.45f;
-//        int total = course_fee + GST; //Narrow - Implicit
+        //int total = course_fee + GST; //Narrow - Implicit
         int total1 = course_fee + (int)GST; //Narrow - Explicit
 
         float total2 = course_fee + GST;
         float total3 = (float)course_fee + GST;
-
-        System.out.println(total1); // widening auto - implicit
+        //System.out.println(total);
+       System.out.println(total1); // widening auto - implicit
         System.out.println(total2); //widening explicit
         System.out.println(total3);
     }
